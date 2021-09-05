17532752cebca51eca6e293bef8847d54f4c79ae
Bulbul0017 <74949576+Bulbul0017@users.noreply.github.com>
2021-09-05 15:34:12 +0530

    Add files via upload

---
+++ to_record_audio.m
@@ -0,0 +1,8 @@
+recObj = audiorecorder
+disp('Start speaking.')
+recordblocking(recObj, 2);
+disp('End of Recording.');
+play(recObj);
+y = getaudiodata(recObj);
+plot(y);
+audiowrite('testfile.wav', y , 8000);
